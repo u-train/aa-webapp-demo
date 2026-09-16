40de7a699be6695ff1867fa402a5fb75279e6d81
utrain <skassem05@outlook.com>
2026-09-16 05:23:49 -0400

    made register use styling

--- resources/js/pages/register.tsx
+++ resources/js/pages/register.tsx
@@ -1,6 +1,7 @@
 import { Head, usePage } from '@inertiajs/react';
 import register from '@/wayfinder/routes/register';
 import NavigationBar from '@/components/navbar';
+import appStyle from '@css/app.module.css';
 
 export default function Register() {
     const {
@@ -15,28 +16,38 @@ export default function Register() {
             <NavigationBar />
 
             <form {...register.store.form()}>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
-                <label htmlFor="password_confirmation">Confirm Password</label>
-                <input
-                    type="password"
-                    name="password_confirmation"
-                    id="password_confirmation"
-                />
-
+                <div className={appStyle.inputEntry}>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" />
+                </div>
+                <div className={appStyle.inputEntry}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div className={appStyle.inputEntry}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div className={appStyle.inputEntry}>
+                    <label htmlFor="password_confirmation">
+                        Confirm Password
+                    </label>
+                    <input
+                        type="password"
+                        name="password_confirmation"
+                        id="password_confirmation"
+                    />
+                </div>
                 {errorEntries.length > 0 && (
-                    <div>
+                    <fieldset className={appStyle.errors}>
+                        <legend>Errors</legend>
                         <p>Could not login because:</p>
                         <ul>
                             {errorEntries.map(([key, error]) => (
                                 <li key={key}>{error}</li>
                             ))}
                         </ul>
-                    </div>
+                    </fieldset>
                 )}
 
                 <button type="submit">Register</button>
