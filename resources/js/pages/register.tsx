9f1fbca3c4c7ad7c9d0e00169940ce2378327b38
utrain <skassem05@outlook.com>
2026-09-16 02:59:21 -0400

    Added labels for registering

--- resources/js/pages/register.tsx
+++ resources/js/pages/register.tsx
@@ -22,9 +22,13 @@ export default function Register() {
             </nav> */}
 
             <form {...register.store.form()}>
+                <label htmlFor="name">Name</label>
                 <input type="text" name="name" id="name" />
+                <label htmlFor="email">Email</label>
                 <input type="email" name="email" id="email" />
+                <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" />
+                <label htmlFor="password_confirmation">Confirm Password</label>
                 <input
                     type="password"
                     name="password_confirmation"
