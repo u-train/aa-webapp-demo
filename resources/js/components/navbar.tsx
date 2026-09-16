536fdfba7b4c70a72b6eeb319cbe0bad1b3a2cf6
utrain <skassem05@outlook.com>
2026-09-16 05:20:32 -0400

    use navurlentry everywhere

--- resources/js/components/navbar.tsx
+++ resources/js/components/navbar.tsx
@@ -1,8 +1,8 @@
 import { useCurrentUserInfo } from '@/hooks/useUserInfo';
 import routes from '@/wayfinder/routes';
-import { Link } from '@inertiajs/react';
 import { ReactNode } from 'react';
 import userRoutes from '@/wayfinder/routes/users';
+import { NavUrlEntry } from './navbar-url-entry';
 
 export default function NavigationBar({ children }: { children?: ReactNode }) {
     const { user, isAdmin } = useCurrentUserInfo();
@@ -10,36 +10,32 @@ export default function NavigationBar({ children }: { children?: ReactNode }) {
     return (
         <nav>
             <ul>
-                <li>
-                    <Link href={routes.home()}>Landing</Link>
-                </li>
-                {!user && (
-                    <li>
-                        <Link href={routes.login()}>Login</Link>
-                    </li>
-                )}
-                {!user && (
-                    <li>
-                        <Link href={routes.register()}>Register</Link>
-                    </li>
-                )}
-                {user && (
-                    <li>
-                        <Link href={routes.logout()}>Logout</Link>
-                    </li>
-                )}
-                {user && (
-                    <li>
-                        <Link href={userRoutes.show(user.email)}>
-                            View My Profile
-                        </Link>
-                    </li>
-                )}
-                {isAdmin && (
-                    <li>
-                        <Link href={userRoutes.index()}>View all Profiles</Link>
-                    </li>
-                )}
+                <NavUrlEntry entry="Landing" url={routes.home().url} />
+                <NavUrlEntry
+                    show={!user}
+                    entry="Login"
+                    url={routes.login().url}
+                />
+                <NavUrlEntry
+                    show={!user}
+                    entry="Register"
+                    url={routes.register().url}
+                />
+                <NavUrlEntry
+                    show={!!user}
+                    entry="Logout"
+                    url={routes.logout().url}
+                />
+                <NavUrlEntry
+                    show={!!user}
+                    entry="View My Profile"
+                    url={userRoutes.show(user?.email ?? '').url}
+                />
+                <NavUrlEntry
+                    show={isAdmin}
+                    entry="View all Profiles"
+                    url={userRoutes.index().url}
+                />
                 {children}
             </ul>
         </nav>
