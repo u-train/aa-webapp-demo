fce05cf17e0e6c7f043636fe55446e3e989c35b8
utrain <skassem05@outlook.com>
2026-09-15 22:29:46 -0400

    brush up the welcome page

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,12 +1,24 @@
-import { Head } from '@inertiajs/react';
-import styles from "@css/app.module.css"
-import clsx from "clsx"
+import routes from '@/wayfinder/routes';
+import userRoutes from '@/wayfinder/routes/users';
+import { Head, Link, usePage } from '@inertiajs/react';
 
 export default function Welcome() {
+    const { props: { isAuthenticated, isAdmin, currentUser } } = usePage()
+
     return (
         <>
             <Head title="Welcome" />
-            <p className={clsx(styles.somethingBig)}> Hello world! </p>
+            <h1>aa-webapp</h1>
+            <nav>
+                <ul>
+                    {!isAuthenticated && <li><Link href={routes.login()}>Login</Link></li>}
+                    {!isAuthenticated && <li><Link href={routes.register()}>Register</Link></li>}
+                    {isAuthenticated && <li><Link href={routes.logout()}>Logout</Link></li>}
+                    {isAuthenticated && currentUser && <li><Link href={userRoutes.show(currentUser.email)}>View Profile</Link></li>}
+                    {isAdmin && <li><Link href={userRoutes.index()}>View all Profiles</Link></li>}
+                </ul>
+            </nav>
+            <p>Welcome to the demo app! Very basic, but functional.</p>
         </>
     );
 }
