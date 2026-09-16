ac9ac50ae6d1df942d0283892e95d09eb3b931af
utrain <skassem05@outlook.com>
2026-09-15 23:14:13 -0400

    Redid profile to be nicer

--- resources/js/pages/profile.tsx
+++ resources/js/pages/profile.tsx
@@ -2,24 +2,23 @@ import { Inertia } from '@/wayfinder/types';
 import { Head, Link, usePage } from '@inertiajs/react';
 
 export default function Profile() {
-    const { props: { isAdmin, isCurrentPage, target_user: user } } = usePage<Inertia.Pages.Profile>()
-
+    const { props: { isAdmin, target_user: targetUser, currentUser } } = usePage<Inertia.Pages.Profile>()
+    const isCurrentUserProfile = targetUser.email === currentUser.email
     return (
         <>
             <Head title="Profile" />
-            <h1>{user.name}'s Profile</h1>
+            <h1>{targetUser.name}'s Profile</h1>
             <nav>
                 <ul>
-                    {/* Without === true, it insists that the type is incorrect here.*/}
-                    {isAdmin === true && <li><Link href="/users">See All Users</Link></li>}
-                    {(isCurrentPage === true || isAdmin) && <li><Link href="/users/">Edit page</Link></li>}
+                    {isAdmin && <li><Link href="/users">See All Users</Link></li>}
+                    {isCurrentUserProfile && <li><Link href="/users/">Edit page</Link></li>}
                     <li><Link method='post' href="/logout">Logout</Link></li>
                 </ul>
             </nav>
 
-            <p>Their email is {user.email}.{" "}
-                {user.updated_at !== null && <>The last time the profile changed was {Temporal.Instant.from(user.updated_at).toLocaleString()}.</>}
-                {user.updated_at === null && <>It is unknown when the profile last changed.</>}
+            <p>Their email is {targetUser.email}.{" "}
+                {targetUser.updated_at !== null && <>The last time the profile changed was {Temporal.Instant.from(targetUser.updated_at).toLocaleString()}.</>}
+                {targetUser.updated_at === null && <>It is unknown when the profile last changed.</>}
             </p>
         </>
     )
